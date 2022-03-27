8a10708090277ed6d25f1ea393bcb90d56f8252a
Blaž Hrastnik <blaz@mxxn.io>
2022-03-27 17:47:33 +0900

    Successfully feature gate DAP

--- helix-term/src/commands/typed.rs
+++ helix-term/src/commands/typed.rs
@@ -816,6 +816,7 @@ fn hsplit_new(
     Ok(())
 }
 
+#[cfg(feature = "dap")]
 fn debug_eval(
     cx: &mut compositor::Context,
     args: &[Cow<str>],
@@ -838,6 +839,7 @@ fn debug_eval(
     Ok(())
 }
 
+#[cfg(feature = "dap")]
 fn debug_start(
     cx: &mut compositor::Context,
     args: &[Cow<str>],
@@ -851,6 +853,7 @@ fn debug_start(
     dap_start_impl(cx, name.as_deref(), None, Some(args))
 }
 
+#[cfg(feature = "dap")]
 fn debug_remote(
     cx: &mut compositor::Context,
     args: &[Cow<str>],
@@ -1345,6 +1348,7 @@ pub const TYPABLE_COMMAND_LIST: &[TypableCommand] = &[
             fun: tree_sitter_scopes,
             completer: None,
         },
+        #[cfg(feature = "dap")]
         TypableCommand {
             name: "debug-start",
             aliases: &["dbg"],
@@ -1352,6 +1356,7 @@ pub const TYPABLE_COMMAND_LIST: &[TypableCommand] = &[
             fun: debug_start,
             completer: None,
         },
+        #[cfg(feature = "dap")]
         TypableCommand {
             name: "debug-remote",
             aliases: &["dbg-tcp"],
@@ -1359,6 +1364,7 @@ pub const TYPABLE_COMMAND_LIST: &[TypableCommand] = &[
             fun: debug_remote,
             completer: None,
         },
+        #[cfg(feature = "dap")]
         TypableCommand {
             name: "debug-eval",
             aliases: &[],
